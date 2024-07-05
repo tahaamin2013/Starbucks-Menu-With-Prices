45a2808900421f97aefe2b06c99471d8faf13947
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-05 17:41:18 +0500

    fds

--- src/components/StarbucksProduct/ProductLayout.tsx
+++ src/components/StarbucksProduct/ProductLayout.tsx
@@ -87,7 +87,7 @@ const ProductLayout = ({ subItem, delay }: any) => {
             aria-label={`Starbucks ${subItem.name}`}
           >
             {" "}
-            <h1 className="text-xl mb-1 w-full md:w-[260px]">{subItem.name}</h1>
+            <h2 className="text-xl mb-1 w-full md:w-[260px]">{subItem.name}</h2>
           </Link>
           <div className="w-44 flex gap-6 justify-between items-center">
             <Link
