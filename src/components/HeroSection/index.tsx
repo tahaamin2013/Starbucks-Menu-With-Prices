889499a3a51cec6af16c470187380850c7c5112c
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-10 05:02:36 +0500

    fds

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -99,28 +99,28 @@ const HeroSection = () => {
   const link = convertNameToLink(selectedProduct.name);
 
   return (
-    <section>
-      <div className="lg:hidden sm:block text-center px-6 mt-8 mb-8">
-    <header>
-      <motion.h1
-        className="font-bold text-gray-900 mb-4 text-5xl"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.1 }} // Adjust the duration to make it faster
-      >
-        Starbucks Menu With Prices 2024
-      </motion.h1>
-      <motion.p
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 0.1, duration: 0.1 }} // Adjust the duration to make it faster
-      >
-        Starbucks offers a diverse menu, including espresso, coffee, tea,
-        bakery items, breakfast, and lunch options. In addition to their
-        specialty coffee drinks, they also provide a selection of snacks and
-        baked goods for those seeking a quick bite.
-      </motion.p>
-    </header> 
+    <section className="">
+      <div className="lg:hidden sm:block text-center px-6 my-8">
+        <header>
+          <motion.h1
+            className="font-bold text-gray-900 mb-4 text-5xl"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.1 }} // Adjust the duration to make it faster
+          >
+            Starbucks Menu With Prices 2024
+          </motion.h1>
+          <motion.p
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.1, duration: 0.1 }} // Adjust the duration to make it faster
+          >
+            Starbucks offers a diverse menu, including espresso, coffee, tea,
+            bakery items, breakfast, and lunch options. In addition to their
+            specialty coffee drinks, they also provide a selection of snacks and
+            baked goods for those seeking a quick bite.
+          </motion.p>
+        </header>
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
@@ -135,142 +135,151 @@ const HeroSection = () => {
         </motion.div>
       </div>
       <MobileHerosection />
-      <div className="md:flex hidden pb-[50px] border-b flex-col md:flex-row py-[40px] md:pl-[90px] xl:pl-[40px] sm:px-0 justify-between items-center">
-        <div className="w-fit md:w-[530px] lg:block hidden">
-          <motion.span
-            className="font-bold text-gray-900 mb-8 text-6xl"
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-          >
-            Starbucks Menu With Prices 2024
-          </motion.span>
-          <motion.p
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ delay: 0.1 }}
-          >
-            Starbucks offers a diverse menu, including espresso, coffee, tea,
-            bakery items, breakfast, and lunch options. In addition to their
-            specialty coffee drinks, they also provide a selection of snacks and
-            baked goods for those seeking a quick bite.
-          </motion.p>
-          <motion.div
-            initial={{ opacity: 0, y: -20 }}
-            animate={{ opacity: 1, y: 0 }}
-          >
-            <GoyButtonforHeroSection
-              id="Menu"
-              classname="bg-transparent border-4 text-primary border-primary w-[11rem] rounded-full duration-500 transition-all mt-8"
-            >
-              View Full Menu
-            </GoyButtonforHeroSection>
-          </motion.div>
-        </div>
-
-        <div className="text-center flex flex-col items-center justify-center gap-3">
-          <motion.div
-            className="ml-0 md:mt-0 mt-5 md:ml-6"
-            initial={{ opacity: 0, scale: 0.8 }}
-            animate={{ opacity: 1, scale: 1 }}
-          >
-            <Image
-              className="rounded-full max-w-[280px] shadow-glow shadow-primary"
-              src={selectedProduct.image}
-              alt={`${selectedProduct.name} Image`}
-              width={425}
-              height={425}
-              loading="lazy"
-            />
-          </motion.div>
-          <div className="mt-2 flex w-full items-center justify-center flex-col">
+      <div className="md:flex hidden pb-[50px] border-b flex-col md:flex-row py-[40px] justify-between items-center">
+        <div className="md:pl-[90px] xl:pl-[40px] pr-16 bg-emerald-400 lg:block hidden">
+          <div className="md:w-[530px]">
             <motion.span
+              className="font-bold text-6xl"
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
-              className="font-bold max-w-xs text-gray-900 mb-3 text-2xl line-clamp-2 h-[60px]"
             >
-              {selectedProduct.name}
+              Starbucks Menu With Prices 2024
             </motion.span>
+            <motion.p
+              className="mt-8"
+              initial={{ opacity: 0, y: -20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.1 }}
+            >
+              Starbucks offers a diverse menu, including espresso, coffee, tea,
+              bakery items, breakfast, and lunch options. In addition to their
+              specialty coffee drinks, they also provide a selection of snacks
+              and baked goods for those seeking a quick bite.
+            </motion.p>
             <motion.div
               initial={{ opacity: 0, y: -20 }}
               animate={{ opacity: 1, y: 0 }}
-              className="flex flex-col gap-2"
             >
-              <Link href={`/articles/${link}`}>
-                <Button className="text-white rounded-full duration-500 transition-all">
-                  View Price & Calories
-                </Button>
-              </Link>
+              <GoyButtonforHeroSection
+                id="Menu"
+                classname="bg-transparent border-4 text-primary border-primary w-[11rem] rounded-full duration-500 transition-all mt-8"
+              >
+                View Full Menu
+              </GoyButtonforHeroSection>
             </motion.div>
           </div>
         </div>
 
-        <div className="flex flex-col items-center justify-center w-[35rem]">
-          <button
-            onClick={handlePreviousSlide}
-            className={`mb-2 ${
-              canScrollPrev ? "bg-primary hover:bg-primary/80" : "bg-gray-300"
-            } rounded-full`}
-            disabled={!canScrollPrev}
-          >
-            <ArrowRight className="h-8 text-white w-8 p-2 -rotate-90" />
-          </button>
-
-          <div
-            className="w-full ml-6 mt-5 max-w-sm overflow-hidden"
-            ref={emblaRef}
-          >
-            <div className="-mt-1 h-[370px]">
-              {allProducts.map((product, index) => {
-                let marginLeftClass = "";
-                switch (index % 3) {
-                  case 0:
-                    marginLeftClass = "pl-[40px]";
-                    break;
-                  case 1:
-                    marginLeftClass = "pl-[80px]";
-                    break;
-                  case 2:
-                    marginLeftClass = "";
-                    break;
-                }
-
-                return (
-                  <div
-                    key={product.link}
-                    className={`pt-1 md:basis-1/3 ${marginLeftClass}`}
-                  >
-                    <button
-                      onClick={() => handleProductClick(product, index)}
-                      className={`${
-                        product === selectedProduct
-                          ? "bg-primary text-white"
-                          : "bg-white"
-                      } w-full text-left text-xl shadow-lg flex gap-3 rounded-r items-center rounded-full px-4 py-2`}
-                    >
-                      <Image
-                        className="rounded-full"
-                        src={product.image}
-                        alt={product.name}
-                        loading="lazy"
-                        width={100}
-                        height={100}
-                      />
-                      <span>{product.name}</span>
-                    </button>
-                  </div>
-                );
-              })}
+        <div className="flex bg-blue-300 w-full justify-end">
+          <div className="text-center flex flex-col items-center justify-center gap-3">
+            <motion.div
+              className="ml-0 md:mt-0 mt-5 md:ml-6"
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+            >
+              <Link href={`/articles/${link}`}>
+                <Image
+                  className="rounded-full max-w-[280px] shadow-glow shadow-primary"
+                  src={selectedProduct.image}
+                  alt={`${selectedProduct.name} Image`}
+                  width={425}
+                  height={425}
+                  loading="lazy"
+                />
+              </Link>
+            </motion.div>
+            <div className="mt-2 flex w-full items-center justify-center flex-col">
+              <Link href={`/articles/${link}`}>
+                <motion.span
+                  initial={{ opacity: 0, y: -20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  className="font-bold max-w-xs mb-3 text-2xl line-clamp-2 h-[60px]"
+                >
+                  {selectedProduct.name}
+                </motion.span>
+              </Link>
+              <motion.div
+                initial={{ opacity: 0, y: -20 }}
+                animate={{ opacity: 1, y: 0 }}
+                className="flex flex-col gap-2"
+              >
+                <Link href={`/articles/${link}`}>
+                  <Button className="text-white rounded-full duration-500 transition-all">
+                    View Price & Calories
+                  </Button>
+                </Link>
+              </motion.div>
             </div>
           </div>
-          <button
-            onClick={handleNextSlide}
-            className={`mt-4 ${
-              canScrollNext ? "bg-primary hover:bg-primary/80" : "bg-gray-300"
-            } rounded-full`}
-            disabled={!canScrollNext}
-          >
-            <ArrowRight className="h-8 text-white w-8 p-2 rotate-90" />
-          </button>
+
+          <div className="flex flex-col items-center justify-center">
+            <button
+              onClick={handlePreviousSlide}
+              className={`mb-2 ${
+                canScrollPrev ? "bg-primary hover:bg-primary/80" : "bg-gray-300"
+              } rounded-full`}
+              disabled={!canScrollPrev}
+            >
+              <ArrowRight className="h-8 text-white w-8 p-2 -rotate-90" />
+            </button>
+
+            <div
+              className="w-full ml-6 mt-5 max-w-sm overflow-hidden"
+              ref={emblaRef}
+            >
+              <div className="-mt-1 h-[370px]">
+                {allProducts.map((product, index) => {
+                  let marginLeftClass = "";
+                  switch (index % 3) {
+                    case 0:
+                      marginLeftClass = "pl-[40px]";
+                      break;
+                    case 1:
+                      marginLeftClass = "pl-[80px]";
+                      break;
+                    case 2:
+                      marginLeftClass = "";
+                      break;
+                  }
+
+                  return (
+                    <div
+                      key={product.link}
+                      className={`pt-1 md:basis-1/3 ${marginLeftClass}`}
+                    >
+                      <button
+                        onClick={() => handleProductClick(product, index)}
+                        className={`${
+                          product === selectedProduct
+                            ? "bg-primary text-white"
+                            : "bg-white"
+                        } w-full text-left text-xl shadow-lg flex gap-3 rounded-r items-center rounded-full px-4 py-2`}
+                      >
+                        <Image
+                          className="rounded-full"
+                          src={product.image}
+                          alt={product.name}
+                          loading="lazy"
+                          width={100}
+                          height={100}
+                        />
+                        <span>{product.name}</span>
+                      </button>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+            <button
+              onClick={handleNextSlide}
+              className={`mt-4 ${
+                canScrollNext ? "bg-primary hover:bg-primary/80" : "bg-gray-300"
+              } rounded-full`}
+              disabled={!canScrollNext}
+            >
+              <ArrowRight className="h-8 text-white w-8 p-2 rotate-90" />
+            </button>
+          </div>
         </div>
       </div>
     </section>
