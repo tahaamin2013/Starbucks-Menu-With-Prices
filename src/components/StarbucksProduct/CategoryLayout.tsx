8b4d2376fad09c69f5e51e355e98cb5a29fd37cf
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-14 00:10:25 +0500

    fds

--- src/components/StarbucksProduct/CategoryLayout.tsx
+++ src/components/StarbucksProduct/CategoryLayout.tsx
@@ -17,28 +17,28 @@ const CategoryLayout = ({ item, delay, key }: any) => {
   });
 
   return (
-      <motion.a
-        ref={ref}
-        initial="hidden"
-        href={item.link}
-        aria-label={`Starbucks ${item.name}`}
-        key={key}
-        animate={inView ? "visible" : "hidden"}
-        variants={variants}
-        transition={{ duration: 0.3, delay }}
-        className="flex gap-8 flex-col md:flex-row items-center"
-      >
-        <Image
-          loading="lazy"
-          decoding="async"
-          src={item.image}
-          alt={`Starbucks ${item.name} Image`}
-          width={120}
-          height={120}
-          className="rounded-full"
-        />
-        <h3 className="text-xl">{item.name}</h3>
-      </motion.a>
+    <motion.a
+      ref={ref}
+      initial="hidden"
+      href={item.link}
+      aria-label={`Starbucks ${item.name}`}
+      key={key}
+      animate={inView ? "visible" : "hidden"}
+      variants={variants}
+      transition={{ duration: 0.3, delay }}
+      className="flex gap-8 flex-col md:flex-row items-left text-left"
+    >
+      <Image
+        loading="lazy"
+        decoding="async"
+        src={item.image}
+        alt={`Starbucks ${item.name} Image`}
+        width={120}
+        height={120}
+        className="rounded-full"
+      />
+      <h3 className="text-xl">{item.name}</h3>
+    </motion.a>
   );
 };
 
