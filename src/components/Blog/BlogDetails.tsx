0ccaf13e8cca5c2988b23dff3ff107c95a3f6e41
tahaamin2013 <tahaamindob2013@gmail.com>
2024-07-14 06:26:39 +0500

    NC

--- src/components/Blog/BlogDetails.tsx
+++ src/components/Blog/BlogDetails.tsx
@@ -4,8 +4,8 @@ const BlogDetails = ({ blog, slug: blogSlug }: { blog: any; slug: any }) => {
   return (
     <section className="mt-2 leading-4 tracking-[2px] font-bold text-slate-400 flex flex-wrap justify-center items-center gap-4">
       <p>by {blog.author}</p>
-
       <time>{format(parseISO(blog.publishedAt), "LLL d, yyyy")}</time>
+      {blog.readingTime.text}
     </section>
   );
 };
